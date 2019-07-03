8223efaeb3b1e339733d93c7ba3852d7a31c5658
Erustus Agutu <erustus.agutu@scivisum.co.uk>
2019-07-03 13:52:22 +0100

    [RD-35360] Fixed argument parser.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,11 +6,19 @@ helpFunction()
    echo -e "\t-t Test type unit/integration/e2e"
    exit 1
 }
-
-getopts "t" CONFIG
-if [ -z "$CONFIG" ]
-then
+while getopts ":t:" o; do
+    case "$o" in
+        t)
+            type=$OPTARG
+            ;;
+        *)
+            helpFunction
+            ;;
+    esac
+done
+if [ -z "$type" ]; then
    echo "Specify a test type";
    helpFunction
 fi
-python -m unittest discover "./tests/$CONFIGtests"
+echo "Running ${type}tests"
+python -m unittest discover ./tests/"$type"tests
