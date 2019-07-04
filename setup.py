81e8c0e17a133e3ed0d3bbace1a1291607a274b0
Mathew Seymour <mathew.seymour@scivisumlt.co.uk>
2019-07-04 13:58:11 +0100

    - Added a short description - LICENSE field is only supposed to contain the license type

--- setup.py
+++ setup.py
@@ -14,9 +14,10 @@ setup(
     name="browserdebuggertools",
     version="1.0.0",
     packages=PACKAGES,
-    license=open("LICENSE.txt").read(),
-    long_description=open("README.md").read(),
+    license="GNU General Public License v3",
+    description="A client which calls remote web browser debugger methods",
     long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
     url="https://github.com/scivisum/browser-debugger-tools",
     author="SciVisum LTD",
     author_email="rd@scivisum.co.uk"
